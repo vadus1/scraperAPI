a8c7a364235390e99bc612382d6f359deed075bf
Pedro Alvarado <pedro.alvarado@globant.com>
2016-08-13 17:59:18 -0500

    Add scraper to Page class and validate url

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,23 @@
 class Page < ActiveRecord::Base
+  validates :url, presence: true
+  validate :url_accessible?, if: "Rails.env.production?"
+
+  before_create :set_content
+
+  def set_content
+    self.content ||= Scraper.new(self.url).call
+  end
+  private :set_content
+
+  def url_accessible?
+    success = begin
+      response = HTTParty.get(self.url)
+      (response.code == 200 || response.code == 302)
+    rescue
+      false
+    end
+    errors.add(:url, 'is not valid or it cannot be accessed at this moment') unless success
+  end
+  private :url_accessible?
+
 end
--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -3,4 +3,25 @@ RSpec.describe Page do
     it { is_expected.to have_attribute :url }
     it { is_expected.to have_attribute :content }
   end
+
+  describe "Validations" do
+    it { is_expected.to validate_presence_of :url }
+    it "expects url is not valid nor accesible" do
+      subject = Page.new(url: 'htttp://www.badurl.com')
+      #subject.valid?
+      # before line fails because spec for 'GET /v1/pages'
+      # instead it is used next line
+      subject.send(:url_accessible?)
+      expect(subject.errors[:url]).to include "is not valid or it cannot be accessed at this moment"
+    end
+  end
+
+  describe "Callbacks" do
+    it "expects to set content attribute before save" do
+      subject = Page.new(url: 'http://www.example.com')
+      subject.send(:url_accessible?)
+      subject.save
+      expect(subject.reload.content).not_to be_nil
+    end
+  end
 end
